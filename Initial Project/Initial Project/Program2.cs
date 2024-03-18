46b338a840a407b4ef428ae29e29d0bcd7e83062
azadkilic0 <a.kilic1213@gmail.com>
2024-03-19 00:25:28 +0100

    Add FindMaximum method to find max value in array

---
+++ Initial Project/Initial Project/Program2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program2
+{
+    static void Main(string[] args)
+    {
+        // Example usage
+        int[] numbers = { 10, 32, 45, 90, 8 };
+        int max = FindMaximum(numbers);
+        Console.WriteLine($"The maximum value is: {max}");
+    }
+
+    static int FindMaximum(int[] nums)
+    {
+        if (nums == null || nums.Length == 0)
+        {
+            throw new ArgumentException("Array cannot be null or empty", nameof(nums));
+        }
+
+        int max = nums[0];
+        foreach (int num in nums)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+        return max;
+    }
+}
