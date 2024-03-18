9a750a041e9af5b0e2f87450b17c3dd8f4cec9d6
azadkilic0 <a.kilic1213@gmail.com>
2024-03-19 00:19:49 +0100

    Implemented CalculateAverage method

--- Initial Project/Initial Project/Program.cs
+++ Initial Project/Initial Project/Program.cs
@@ -1,4 +1,33 @@
-﻿
-Console.WriteLine("Modification 1");
-Console.WriteLine("Modification 2");
-Console.WriteLine("Modification 3");
+﻿using System;
+
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    
+    {
+        Console.WriteLine("Modification 1");
+        Console.WriteLine("Modification 2");
+        Console.WriteLine("Modification 3");
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        double average = CalculateAverage(numbers);
+        Console.WriteLine($"The average is: {average}");
+    }
+
+    static double CalculateAverage(int[] nums)
+    {
+        if (nums == null || nums.Length == 0)
+        {
+            throw new ArgumentException("Array cannot be null or empty", nameof(nums));
+        }
+
+        long sum = 0;
+        foreach (int num in nums)
+        {
+            sum += num;
+        }
+        return (double)sum / nums.Length;
+    }
+}
